4fbe3f108db5b341acf15c4ea64d3c12ef0b5f5f
Phil Goo Kang <philgookang@gmail.com>
2018-11-10 02:27:30 +0900

    added lz78 decoding function

--- lz78.py
+++ lz78.py
@@ -16,38 +16,67 @@ class LZ78:
         node_id = 1
         character_string = ""
 
-        with open(input_file) as ins:
-            for line in ins:
-                for character in line:
+        # with open(input_file) as ins:
+        #     for line in ins:
+        line = "aaabbabaabaaabab"
+        #       aaabbabaabaaabab
+        for character in line:
 
-                    is_found = self.rootNode.searchChild(character_string + character)
+            is_found = self.rootNode.searchChildByEdge(character_string + character)
 
-                    if not is_found:
-                        if self.dev:
-                            print((node_id, character_string + character))
+            if not is_found:
+                if self.dev:
+                    print((node_id, character_string + character))
 
-                        # create child node to add
-                        new_child_to_add = Node(id=node_id, edge=character)
+                # create child node to add
+                new_child_to_add = Node(id=node_id, edge=character)
 
-                        # add new child to trie
-                        # get update child with parent_id
-                        new_child_to_add = self.rootNode.addChild(new_child_to_add, character_string)
+                # add new child to trie
+                # get update child with parent_id
+                new_child_to_add = self.rootNode.addChild(new_child_to_add, character_string)
 
-                        # save output
-                        self.output.append((new_child_to_add.parent_id, new_child_to_add.edge))
+                # save output
+                self.output.append((new_child_to_add.parent_id, new_child_to_add.edge))
 
-                        character_string = ""
-                        node_id += 1
-                    else:
-                        character_string += character
+                character_string = ""
+                node_id += 1
+            else:
+                character_string += character
+
 
     def decrypt(self, output_file):
 
+        # create out variable
         output = self.output
 
-        rootNode = Node(id=0, is_root=True)
+        # original final string
+        original_string = ""
 
-        # for o in output:
+        # loop through output list
+        for idx,o in enumerate(output):
+
+            # check for node in TRIE that has this parent id
+            search_result = self.rootNode.addChildByParentId((idx + 1), o)
+
+            # temp save current node to root path
+            statement = ""
+
+            # loop through all the edges
+            while search_result.edge:
+                # save edge value
+                statement = search_result.edge + statement
+
+                # change next node
+                search_result = search_result.parent_node
+
+            # add temp statement to final string list
+            original_string = original_string + statement
+
+            if self.dev:
+                print(statement)
+
+        if self.dev:
+            print(original_string)
 
 
     def log(self):
@@ -56,8 +85,12 @@ class LZ78:
 
 input_file = "infile.txt"
 
-lz = LZ78()
-lz.encrypt(input_file)
-lz.decrypt(input_file)
-lz.log()
+enc_lz = LZ78()
+dec_lz = LZ78()
+
+enc_lz.encrypt(input_file)
+
+dec_lz.output = enc_lz.output
+dec_lz.decrypt(input_file)
+# lz.log()
 
--- node.py
+++ node.py
@@ -7,6 +7,9 @@ class Node:
     # parent node #
     parent_id = None
 
+    # parent node pointer
+    parent_node = None
+
     # edge value
     edge = None
 
@@ -17,15 +20,16 @@ class Node:
     child_node_list = None
 
 
-    def __init__(self, parent_id = None, id = None, edge = None, is_root = False):
+    def __init__(self, parent_id = None, parent_node = None, id = None, edge = None, is_root = False):
         self.id = id
         self.parent_id = parent_id
+        self.parent_node = parent_node
         self.edge = edge
         self.is_root = is_root
         self.child_node_list = list()
 
 
-    def searchChild(self, character_string):
+    def searchChildByEdge(self, character_string):
 
         # length of the search character string
         len_of_char_str = len(character_string)
@@ -47,7 +51,32 @@ class Node:
 
                 # found child but still more to search
                 else:
-                    return child.searchChild(character_string[1:])
+                    return child.searchChildByEdge(character_string[1:])
+
+        # not found!
+        return False
+
+
+    def searchChildByParentId(self, parent_id):
+
+        # exception handler for when looking for 0 parent id
+        if not parent_id:
+            return self
+
+        # loop through all child to see if their value equals search
+        for child in self.child_node_list:
+
+            # check if child id is equal to search parent id
+            if child.id == parent_id:
+                return child
+            # found child but still more to search
+            else:
+                # check child subsearch
+                return_from_recursive_search = child.searchChildByParentId(parent_id)
+
+                # if child found, return or else, go loop next interation
+                if return_from_recursive_search:
+                    return return_from_recursive_search
 
         # not found!
         return False
@@ -55,26 +84,37 @@ class Node:
 
     def addChild(self, new_child, character_string):
 
-        # length of the search character string
-        len_of_char_str = len(character_string)
+        # set default last child as me!
+        last_child = self
 
-        # search string is zero 0
-        if not len_of_char_str:
-
-            # set root id
-            new_child.parent_id = self.id
-
-            # add child root child list
-            self.child_node_list.append(new_child)
-        else:
+        # search when the string is not zero!
+        if len(character_string):
             # get last child
-            last_child = self.searchChild(character_string)
+            last_child = self.searchChildByEdge(character_string)
 
-            # set new child parent id
-            new_child.parent_id = last_child.id
+        # set new child parent id
+        new_child.parent_id = last_child.id
 
-            # added new child to parent
-            last_child.child_node_list.append(new_child)
+        # set new child parent node pointer
+        new_child.parent_node = last_child
+
+        # added new child to parent
+        last_child.child_node_list.append(new_child)
 
         # return child for logging
         return new_child
+
+
+    def addChildByParentId(self, idx, o):
+
+        # check for node in TRIE that has this parent id
+        search_result = self.searchChildByParentId(o[0])
+
+        # create child node to add
+        new_child_to_add = Node(parent_id=o[0], parent_node=search_result, id=idx, edge=o[1])
+
+        # add child to parent node
+        search_result.child_node_list.append(new_child_to_add)
+
+        # return child because edge data start's with child node
+        return new_child_to_add
