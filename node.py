0e0783ae74b77012827030e74f4d009f3e919c15
Phil Goo Kang <philgookang@gmail.com>
2018-11-13 22:32:38 +0900

    Refector code, removed unused functions

--- node.py
+++ node.py
@@ -29,15 +29,7 @@ class Node:
         self.child_node_list = dict()
 
 
-    def searchChildByEdge(self, character_string):
-
-        # length of the search character string
-        len_of_char_str = len(character_string)
-
-        # always check first character of search string
-        # because this is a recursive loop
-        # empty string exception will be handled on call
-        character = character_string[0]
+    def searchChildByEdge(self, character):
 
         # check if edge exsits
         for child in self.child_node_list:
@@ -47,90 +39,16 @@ class Node:
 
             # check if child edge is equal to search character
             if child.edge == character:
-
-                # found last child in the search
-                if len_of_char_str == 1:
-                    return child
-
-                # found child but still more to search
-                else:
-                    return child.searchChildByEdge(character_string[1:])
+                return child
 
         # not found!
         return False
 
 
-    def searchChildByParentId(self, parent_id):
-
-        # exception handler for when looking for 0 parent id
-        if not parent_id:
-            return self
-
-        # loop through all child to see if their value equals search
-        if parent_id in self.child_node_list:
-            return self.child_node_list[parent_id]
-
-        # not found then go one depth more
-        else:
-
-            # loop through child to see if they have the node
-            for k in self.child_node_list:
-
-                # check if parent id is larger then child
-                if k <= parent_id:
-
-                    # get node child
-                    child = self.child_node_list[k]
-
-                     # check child subsearch
-                    return_from_recursive_search = child.searchChildByParentId(parent_id)
-
-                     # if child found, return or else, go loop next interation
-                    if return_from_recursive_search:
-
-                        # node found, lets return it!
-                        return return_from_recursive_search
-        # not found!
-        return False
-
-
-    def addChild(self, new_child, character_string):
-
-        # set default last child as me!
-        last_child = self
-
-        # search when the string is not zero!
-        if len(character_string):
-            # get last child
-            last_child = self.searchChildByEdge(character_string)
-
-        # set new child parent id
-        new_child.parent_id = last_child.id
-
-        # set new child parent node pointer
-        new_child.parent_node = last_child
-
+    def addChild(self, new_child):
 
         # added new child to parent
-        last_child.child_node_list[new_child.id] = new_child
-
+        self.child_node_list[new_child.id] = new_child
 
         # return child for logging
         return new_child
-
-
-    def addChildByParentId(self, idx, o):
-
-        # check for node in TRIE that has this parent id
-        search_result = self.searchChildByParentId(o[0])
-
-        # create child node to add
-        new_child_to_add = Node(parent_id=o[0], parent_node=search_result, id=idx, edge=o[1])
-
-        # add child to parent node
-        # search_result.child_node_list.append(new_child_to_add)
-        search_result.child_node_list[new_child_to_add.id] = new_child_to_add
-
-        # return child because edge data start's with child node
-        return new_child_to_add
-
