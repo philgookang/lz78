19366e20657115e1bdef74950b342f4c4ed710ae
Phil Goo Kang <philgookang@gmail.com>
2018-11-13 22:33:12 +0900

    Changed tree search to start at last node not the root

--- lz78.py
+++ lz78.py
@@ -4,24 +4,31 @@ import time
 
 class LZ78:
 
-    dic = None
-    rootNode = None
-    output = None
-    dev = False
+    dic         = None
+    rootNode    = None
+    output      = None
+
 
     def __init__(self):
         self.output = list()
         self.dic = list()
         self.rootNode = Node(id=0, is_root=True)
 
-    def encrypt(self, input_file):
 
-        node_id = 1
-        character_string = ""
+    def encrypt(self, input_file):
 
         io = Fileio()
         text_list = io.read_text(input_file)
         # text_list = ["aaabbabaabaaabab"]
+
+        # set search start node to root as default
+        search_start_node = self.rootNode
+
+        # default start node id is 1
+        node_id = 1
+
+        # set cummulative string variable
+        character_string = ""
 
         # start encoding time
         start_time = time.time()
@@ -30,23 +37,29 @@ class LZ78:
         for line in text_list:
             for character in line:
 
-                is_found = self.rootNode.searchChildByEdge(character_string + character)
+                # check in dic if n
+                is_found = search_start_node.searchChildByEdge(character)
 
                 if not is_found:
 
                     # create child node to add
-                    new_child_to_add = Node(id=node_id, edge=character)
+                    new_child_to_add = Node(parent_id=search_start_node.id, parent_node=search_start_node, id=node_id, edge=character)
 
                     # add new child to trie
                     # get update child with parent_id
-                    new_child_to_add = self.rootNode.addChild(new_child_to_add, character_string)
+                    new_child_to_add = search_start_node.addChild(new_child_to_add)
 
                     # save output
                     self.output.append((new_child_to_add.parent_id, new_child_to_add.edge))
 
-                    character_string = ""
-                    node_id += 1
+                    character_string = ""               # reset search character string
+                    node_id += 1                        # set next node id
+                    search_start_node = self.rootNode   # reset start node to the top root
                 else:
+                    # set next search start node
+                    search_start_node = is_found
+
+                    # cummulative character string
                     character_string += character
 
         # start encoding time
@@ -100,11 +113,13 @@ class LZ78:
         # start encoding time
         end_time = time.time()
 
-        print('encoding time: ', (end_time - start_time))
+        print('decoding time: ', (end_time - start_time))
+
 
     def save(self, output_filename):
         io = Fileio()
         io.save_binary(output_filename, self.output)
+
 
     def open(self, input_file):
         io = Fileio()
