e31803d01e61e3fee847ca0539aac9837ba89412
Phil Goo Kang <philgookang@gmail.com>
2018-11-20 10:49:02 +0900

    Changed saving method from big endian to binary

--- fileio.py
+++ fileio.py
@@ -1,6 +1,5 @@
-import math
-
 from util import *
+from bitarray import bitarray
 
 class Fileio:
 
@@ -11,68 +10,6 @@ class Fileio:
                 return_list.append(line)
         return return_list
 
-
-    def read_binary(self, input_file):
-
-        file = open(input_file, 'rb')
-        encoded = file.read()
-
-        tmp_output_list = list()
-
-        x_size = 0  # number of consecutive bytes which need to be combined
-        write_b = None  # buffer for temporal bytes
-
-        node_id = ""
-        node_edge = ""
-
-        for byte in encoded:
-
-            # x_size == 0 means that previous data successfully decoded and added to result list 'st'
-            # decode next binary data
-            if x_size == 0:
-                # check 2 MSBs to know how many bytes comprises a codeword
-                header = byte & 192
-                header = header >> 6
-
-                # reset number bits for precise calculations
-                if byte >= 128:
-                    byte -= 128
-                if byte >= 64:
-                    byte -= 64
-
-                # convert int to bytes for concatenation with consecutive bytes
-                write_b = byte.to_bytes(1,'big')
-
-                # number of bytes of a codeword
-                x_size = header + 1
-
-            else:
-                # concatenate consecutive byte
-                write_b += byte.to_bytes(1,'big')
-
-            x_size -= 1
-
-            # if x_size = 0, add write_b to st list
-            if x_size == 0:
-
-                if node_id == "":
-                    node_id = int.from_bytes(write_b,'big')
-                elif node_edge == "":
-                    node_edge = chr(int.from_bytes(write_b, 'big'))
-
-                    c = (node_id, node_edge)
-                    tmp_output_list.append(c)
-
-                    node_id = ""
-                    node_edge = ""
-
-                # convert bytes to int and add the result list
-                # tmp_output_list.append(int.from_bytes(write_b,'big'))
-                write_b = None # initialize buffer
-
-        return tmp_output_list
-
-
     def save_text(self, filename, string_to_save):
         file = open(filename, 'w')
         file.write(string_to_save)
@@ -81,22 +18,68 @@ class Fileio:
 
     def save_binary(self, filename, output):
 
-        file = open(filename, "wb")
+        binary_list = bitarray()
 
-        for tuple_item in output:
+        for idx, o in enumerate(output):
 
-            node_id = tuple_item[0]
-            node_edge = ord(tuple_item[1])
+            size = Util().calculateBinaryRequired(idx)
 
-            # encode integer as bytes. Big Endian
-            node_id_bytes = Util().encode(node_id)
-            node_edge_bytes = Util().encode(node_edge)
+            node_id = o[0]
+            node_edge = o[1]
 
-            # save big endian string encoded
-            file.write(node_id_bytes)
+            node_id_bin_format = '{:0' + str(size) + 'b}'
+            node_id_bin = node_id_bin_format.format(node_id)
 
-            # save big endian string encoded
-            file.write(node_edge_bytes)
+            binary_list.extend(node_id_bin)
+            binary_list.extend(Util().fillZeroCharacter(node_edge))
 
-        # close file
-        file.close()
+        with open(filename, 'wb') as fh:
+            binary_list.tofile(fh)
+
+
+    def read_binary(self, input_file, enc_lz):
+
+        binary_list = bitarray()
+
+        with open(input_file, 'rb') as fh:
+            binary_list.fromfile(fh)
+
+        start = 0
+        end = 1
+        size = 1
+        idx = 0
+
+        decode_output_list = []
+
+        binary_list_item_count = len(binary_list)
+
+        tmp = []
+
+        while True:
+
+            if start >= binary_list_item_count:
+                break
+
+            size = Util().calculateBinaryRequired((idx + 1))
+
+            node_id = binary_list[start:end]
+            node_edge = binary_list[end:(end + 7)]
+
+            start = (end + 7)
+            end = start + size
+
+            tmp.append([node_id, node_edge])
+
+            try:
+                o1 = int(node_id.to01(), 2)
+                o2 = chr(int(node_edge.to01(), 2))
+
+                a = (o1, o2)
+                decode_output_list.append(a)
+
+            except ValueError as e:
+                break
+
+            idx += 1
+
+        return decode_output_list
